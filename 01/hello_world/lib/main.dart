614b8724aa7b7edd27e05ffd5364d16378de596e
Raj Ratan <raj808341@gmail.com>
2023-06-13 14:52:51 +0530

    first hello world in Flutter

---
+++ 01/hello_world/lib/main.dart
@@ -0,0 +1,31 @@
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Hello World',
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: const Text('Hello World'),
+//         ),
+//         body: const Center(
+//           child: Text('Hello, World!'),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
+// First Hello World From LCOss
+// import 'package:flutter/widgets.dart';
+
+// void main() => runApp(
+//     const Center(child: Text('Hello World', textAlign: TextAlign.center)));
